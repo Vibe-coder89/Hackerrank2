a5e9cdd6d51b65204a946a3c509f1b33567bdc11
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-08 19:53:37 +0530

    \

--- 43.cpp
+++ 43.cpp
@@ -1,3 +1,23 @@
+Write a C++ program to demonstrate Hybrid Inheritance. Create the following classes: Base Class Vehicle • vehicleId • mileage
+
+Derived Class 1 Car (inherits from Vehicle) • Display vehicle details Another Base Class Fare • farePerKm Derived Class 2 Bus (inherits from Vehicle and Fare) • Calculate total fare = mileage × farePerKm • Display vehicle details and total fare
+
+Class Structure Vehicle / \ Car Bus \ Fare
+
+Input Format
+
+First line: Integer N (number of vehicles) Next N lines: VehicleType VehicleID Mileage FarePerKm Where: • VehicleType = C (Car) or B (Bus) • VehicleID = integer • Mileage = integer • FarePerKm = integer (only used for Bus, for Car it can be 0)
+
+Constraints
+
+Constraints • 1 ≤ N ≤ 50 • 100 ≤ VehicleID ≤ 9999 • -10 ≤ Mileage ≤ 200 • 1 ≤ FarePerKm ≤ 50 • VehicleType must be either C or B
+
+Output Format
+
+For Car: Vehicle ID: Mileage: Type: Car For Bus: Vehicle ID: Mileage: Fare Per Km: Total Fare: Type: Bus If mileage ≤ 0 → print Invalid Mileage
+
+
+
 #include <iostream>
 using namespace std;
 
