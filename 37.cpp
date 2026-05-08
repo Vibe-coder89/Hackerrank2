84670ca48691988328642a2e4aa41869825548e9
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-08 19:46:49 +0530

    Update 37.cpp

--- 37.cpp
+++ 37.cpp
@@ -1,3 +1,21 @@
+You are given an integer N. Create N objects of the Derived class.
+
+Each time an object is created: • First the Base constructor must execute. • Then the Derived constructor must execute. When the program finishes, destructors will be called automatically in reverse order: • First the Derived destructor • Then the Base destructor Print the order of constructor and destructor calls
+
+Input Format
+
+A single integer N
+
+Constraints
+
+• 1 ≤ N ≤ 5 • Use inheritance only • Output must match exactly
+
+Input = n (number of objects) We must check: • n must be a number • n must be between 1 and 5 • If invalid → print "Invalid Input"
+
+Output Format
+
+For every object created: • Print constructor messages • After all objects are created, destructors should be printed automatically in reverse order.
+
 #include <iostream>
 using namespace std;
 
