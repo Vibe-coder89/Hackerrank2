415e45fba34b1cddabeade1605b0fbe43d57d686
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-08 19:40:20 +0530

    Update 32.cpp

--- 32.cpp
+++ 32.cpp
@@ -1,3 +1,21 @@
+You are given an integer value representing the number of minutes. Create a class Time that converts this integer into an object representing hours and minutes. Use a conversion constructor to convert the integer value into a Time object. Finally, display the time in the format: Hours: H Minutes: M
+
+Description: In C++, a conversion constructor allows a basic data type (like int) to be converted into an object of a class. For this problem: • The input will be a single integer N representing total minutes • Convert this integer into a Time object • Extract: o Hours = N / 60 o Minutes = N % 60 • Print the converted time Validations: if user enters non-integer value => Invalid input! Please enter a valid number negative minutes not allowed => Invalid input! Minutes cannot be negative Only valid input goes to the constructor
+
+Input Format
+
+A single integer N representing total minutes Required Class Design
+
+You must use: class Time { int hours; int minutes; public: Time(int totalMinutes); // Conversion constructor void display(); };
+
+Constraints
+
+1 ≤ N ≤ 100000
+
+Output Format
+
+Print the converted time in the format: Hours: H Minutes: M Or Invalid input! Please enter a valid number Invalid input! Minutes cannot be negative
+
 #include <iostream>
 using namespace std;
 
