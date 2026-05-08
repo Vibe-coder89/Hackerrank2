23a66b9387f15de4a055da2107f48338ecf61ce6
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-09 00:47:01 +0530

    Update 60.cpp

--- 60.cpp
+++ 60.cpp
@@ -18,42 +18,21 @@ using namespace std;
 int main() {
 
     int balance, amount;
-
     cin >> balance >> amount;
 
-    try {
-
-        
-        if(amount <= 0) {
-            throw "Invalid";
-        }
-
-        
-        else if(amount > balance) {
-            throw 1;
-        }
-
-        
-        else if(balance < 0) {
-            throw 10.5;
-        }
-
-        else {
-            cout << "Please collect your cash";
-        }
-    }
-
-    catch(int) {
-        cout << "Insufficient balance";
-    }
-
-    catch(const char*) {
+    if(amount <= 0) {
         cout << "Invalid withdrawal amount";
     }
-
-    catch(...) {
+    else if(amount > balance) {
+        cout << "Insufficient balance";
+    }
+    else if(balance < 0) {
         cout << "ATM system error! Please try again later.";
+    }
+    else {
+        cout << "Please collect your cash";
     }
 
     return 0;
+}
 }
