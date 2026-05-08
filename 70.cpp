cb288de380c539b87813f089dfd2628e36d5f8ef
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-08 20:17:24 +0530

    Update 70.cpp

--- 70.cpp
+++ 70.cpp
@@ -1,3 +1,26 @@
+
+Using STL Vectors Given a sentence, reverse the order of words. Using StringStream for handling each word using space delimeter and storing into container vectors and showing it into reverse order Iterators
+
+Input Format: • A single line string s
+
+Output Format: • Print reversed sentence
+
+Constraints: • 1≤ ∣ s ∣≤ 10^51
+
+Input Format
+
+Input Format: • A single line string s
+
+Constraints
+
+Constraints: • 1≤ ∣ s ∣≤ 10^51
+
+1 I love C++ C++ love I 2 Hello World World Hello 3 One Two Three Three Two One 4 a b c c b a 5 Coding is fun fun is Coding
+
+Output Format
+
+Output Format: • Print reversed sentence
+
 #include <iostream>
 #include <vector>
 #include <sstream>
