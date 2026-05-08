12f213b7e4d4e014b11a438330e7275c0c118f75
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-08 19:39:57 +0530

    Update 31.cpp

--- 31.cpp
+++ 31.cpp
@@ -1,3 +1,29 @@
+You are given multiple patient records. Each patient object stores: • Patient ID • Patient Age • Number of days admitted in hospital Your task is to: 1. Create a class Patient 2. Store all patient details inside the class 3. Implement a conversion operator that converts a Patient object into a basic integer representing the total hospital cost
+
+Cost Calculation Rule: Total Cost = Age × Days Admitted
+
+The program must: • Read multiple patient records • Convert each object into an integer using the conversion operator • Print the total cost for each patient
+
+Description This problem tests: • Object to basic type conversion operator • Multiple object handling • Array of objects • Real-world scenario (hospital billing) • HackerRank-style input/output handling
+
+Required Conversion Operator You must use: operator int()
+
+What Validation Does If any value is outside the allowed range: • Program stops immediately • Displays an error message
+
+Input Format
+
+First line contains integer N (number of patients) Next N lines contain: PatientID Age DaysAdmitted
+
+If N not in valid range validation error => Invalid number of patients If Id,Age,days not in valid range validation error => Invalid input for patient 1
+
+Constraints
+
+1 ≤ N ≤ 100 1 ≤ PatientID ≤ 10000 0 ≤ Age ≤ 120 1 ≤ DaysAdmitted ≤ 365
+
+Output Format
+
+Print the total hospital cost for each patient in a new line
+
 #include <iostream>
 using namespace std;
 
