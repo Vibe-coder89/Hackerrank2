9cb7aad3f490147ff3430f7d449c451f0e2b4488
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-08 19:38:20 +0530

    Add Number class with overloaded operators for negation and addition
    Implemented a Number class with overloaded unary and binary operators to handle negation and addition. Updated main function to demonstrate functionality with user input.

--- 28.cpp
+++ 28.cpp
@@ -1,3 +1,21 @@
+Design a class Number in C++ that overloads: 1. Unary Operator (-) o To negate the value of an object. o Must be implemented using a friend function. 2. Binary Operator (+) o To add two Number objects. o Must be implemented using a friend function. You are required to: • Read two integer values. • Create two objects of class Number. • Perform: o Unary minus on first object o Addition of two objects • Print results in the specified format.
+
+Input Format
+
+Two space-separated integers: a b
+
+Constraints
+
+• −106≤a,b≤106-10^6
+• Must use friend function for both operators. • No global variables allowed.
+
+Output Format
+
+Print in the following format:
+
+Negation of First Number: Sum:
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
