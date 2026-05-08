cc4afb7719229e8f2dc8b1a8d9723e9bde952a2c
Shivam Khajuria <forgithub34d4@gmail.com>
2026-05-08 19:50:43 +0530

    Update 40.cpp

--- 40.cpp
+++ 40.cpp
@@ -1,3 +1,21 @@
+Create the following classes: 1. Class Academics • Stores marks of 3 subjects • Validate marks (0–100) 2. Class Sports • Stores sports marks • Validate marks (0–50) 3. Class Result This class should inherit from both: Academics + Sports → Result The Result class must: • Calculate total marks • Calculate percentage • Display the final result
+
+Input 101 70 80 90 40
+
+Output ID: 101 Academic Total: 240 Sports Marks: 40 Grand Total: 280 Percentage: 80
+
+Input Format
+
+id m1 m2 m3 sportsMarks
+
+Constraints
+
+1 ≤ id ≤ 1000 0 ≤ m1, m2, m3 ≤ 100 0 ≤ sportsMarks ≤ 50
+
+Output Format
+
+ID: Academic Total: Sports Marks: Grand Total: Percentage: If marks are invalid: Invalid Marks
+
 #include <iostream>
 using namespace std;
 
